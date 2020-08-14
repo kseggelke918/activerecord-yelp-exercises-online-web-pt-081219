5e7c714b30ffc2347c0dad025ce594e91f60e320
kseggelke918 <katie.seggelke@gmail.com>
2020-08-14 14:31:08 -0600

    add validation for number of words

--- yelp-practice-app/app/models/dish.rb
+++ yelp-practice-app/app/models/dish.rb
@@ -2,5 +2,6 @@ class Dish < ApplicationRecord
     has_many :tags
     belongs_to :restaurant
     validates :name, presence: true
+    validates :tag, uniqueness: true 
     # validates :restaurant, presence: true
 end
--- yelp-practice-app/app/models/tag.rb
+++ yelp-practice-app/app/models/tag.rb
@@ -1,5 +1,9 @@
 class Tag < ApplicationRecord
     has_many :dishes
     validates :name, presence: true
-    validates :name, length: { minimum: 3 }
+    validates :name, length: { 
+        minimum: 3, 
+        tokenizer: lambda { |str| str.scan(/\w+/) }, 
+        too_long: "cannot have more than 2 words" 
+    }
 end
