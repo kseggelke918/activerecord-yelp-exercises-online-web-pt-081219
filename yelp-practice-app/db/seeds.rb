8885f5baa730864d9989d619e8ab2b36e32ef9da
kseggelke918 <katie.seggelke@gmail.com>
2020-08-17 15:37:57 -0600

    start instance creation in seed file

--- yelp-practice-app/db/seeds.rb
+++ yelp-practice-app/db/seeds.rb
@@ -8,4 +8,16 @@
 
 restaurant_list = ["Q House", "Arcana", "Safta", "Dio Mio", "Cattivella", "12 at Madison", "Spuntino", "Acorn", "The Wolf's Tailor", "Hop Alley". "Julep", "Annette", "Blackbelly", "Beast and Bottle", "Comal Heritage Food Incubator", "Mercantile Dining & Provision", "Barolo Grill", "Corrid", "Frasca Food & Wine", "Morin", "Tavernetta", "LeRoux", "Super Mega Bien", "Beckon", "Uchi"]
 tags = ["spicy", "vegan", "gluten free", "vegetarian", "mild", "specialty", "low calorie", "low carb"]
-restaurant_dishes = ["roasted chicken", "beef tenderloins", "steak", "tacos", "beef stir fry", "chicken stir fry", "hamburgers", "summer salad", "ceasar salad", "apple pie", "chocolate cake", "fajita burrito", "spaghetti", "tortellini", "italian sausage", "salmon", "trout", "snapper", "prime rib", "chicken and waffles", "ice cream", "calamari", "brushetta", "mozzarella sticks", "soup of the day", "chopped salad"]
+dishes = ["roasted chicken", "beef tenderloins", "steak", "tacos", "beef stir fry", "chicken stir fry", "hamburgers", "summer salad", "ceasar salad", "apple pie", "chocolate cake", "fajita burrito", "spaghetti", "tortellini", "italian sausage", "salmon", "trout", "snapper", "prime rib", "chicken and waffles", "ice cream", "calamari", "brushetta", "mozzarella sticks", "soup of the day", "chopped salad"]
+
+tags.each do |t| 
+    Tag.create(name: t)
+end 
+
+dishes.each to |dish_name|
+    Dish.create(name: dish_name)
+end 
+
+restaurant_list.each do |name| 
+    Restaurant.create(name: name)
+end 
